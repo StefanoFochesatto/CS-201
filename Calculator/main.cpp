03cc485503e090f300e3c14f73fb8d3342756c71
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-08-28 16:26:39 -0800

    This is a test

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,15 +1,50 @@
-//
+////
 //  main.cpp
-//  Calculator
+//  Arithmatic
 //
-//  Created by Stefano Fochesatto on 8/28/19.
+//  Created by Stefano Fochesatto on 8/27/19.
 //  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+//
+//
+//
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    using namespace std;
+    cout << "Hello! What operation would you like to perform?\n";
+    //Initilizing the the tring that represents the operation
+    string operation;
+    cin >> operation;
+    cout << "Enter two integers to operate?\n";
+    //initilizing each integer.
+    int number1;
+    int number2;
+    cin >> number1;
+    cin >> number2;
+    // If statement for each arithmetic operation
+    if (operation == "+")
+    {
+        cout <<"Adding "<<number1<<" and "<<number2<<"."<<" Result:"<<number1+number2<<". \n"  ;
+    }
+    if (operation == "-")
+    {
+        cout <<"Subtracting "<<number2<<" from "<<number1<<"."<<" Result:"<<number1-number2<<".\n"  ;
+    }
+    if (operation == "*")
+    {
+        cout <<"Multiplying "<<number1<<" and "<<number2<<"."<<" Result:"<<number1*number2<<".\n"  ;
+    }
+    
+    if (operation == "/")
+    {
+        cout <<"Dividing "<<number1<<" over "<<number2<<"."<<" Result:"<<number1/number2<<".\n"  ;
+        
+    }
     return 0;
 }
+
+
+
