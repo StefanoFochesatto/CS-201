3cdbb619a2d574225894c765b6a144083ce9889b
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-11 10:04:20 -0800

    LAB 04

--- Labs/L04/More Iteration/More Iteration/main.cpp
+++ Labs/L04/More Iteration/More Iteration/main.cpp
@@ -8,8 +8,61 @@
 
 #include <iostream>
 
+// Additional lab
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    for(int i=0; i<=99; i++){
+        
+        if(((i+1)%5)==0&&((i+1)%3)==0){
+            std::cout << "fizzbuzz" << std::endl;
+            
+        }
+        else if((i+1)%5==0){
+            std::cout << "buzz" << std::endl;
+        }
+        else if((i+1)%3==0){
+        std::cout << "fizz" << std::endl;
+        
+        }
+        else{
+            std::cout << i+1 << std::endl;
+    
+        }
+    }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+int main(int argc, const char * argv[]) {
+   
+    for(int i=0; i<=99; i++){
+        if((i+1)%3)
+        {
+            std::cout << i+1 << std::endl;
+        }
+        else{
+            std::cout << "fizz" << std::endl;
+            
+        }
+    }
+    return 0;
+}
+ **/
