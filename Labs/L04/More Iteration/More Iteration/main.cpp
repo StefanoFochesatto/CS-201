d02a01131e0fa523fb1a7d25f80038b8de4f68b3
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-11 10:03:38 -0800

    Lab 04 completed

---
+++ Labs/L04/More Iteration/More Iteration/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  More Iteration
+//
+//  Created by Stefano Fochesatto on 9/11/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
