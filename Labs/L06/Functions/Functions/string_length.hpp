f93932c1485524c21376626af3cad3d9633dd4be
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-16 10:21:16 -0800

    Lab 04

---
+++ Labs/L06/Functions/Functions/string_length.hpp
@@ -0,0 +1,14 @@
+//
+//  string_length.hpp
+//  Functions
+//
+//  Created by Stefano Fochesatto on 9/16/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#ifndef string_length_hpp
+#define string_length_hpp
+
+#include <stdio.h>
+
+#endif /* string_length_hpp */
