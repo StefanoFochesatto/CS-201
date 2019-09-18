eb41ebb1af3be0b787a8f4acf18055c82deb169d
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-18 10:18:03 -0800

    Lab07

--- Labs/L07/Calling Function/Calling Function/main.cpp
+++ Labs/L07/Calling Function/Calling Function/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
-
+#include "intio.hpp"
+#include <vector>
+#include <numeric>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Type an integer: ";
+    std::vector<int> v;
+    int j = 1;
+   do {
+        j = getInt();
+        v.push_back(j);
+   } while (j != 0);
+    int i = accumulate(v.begin(),v.end(),0);
+    putInt(i, 10);
+
+    
     return 0;
 }
+
+//int num1 = getInt();
+//int num2 = getInt();
+//int num3 = getInt();
+
+//putInt(num1, 10);
+//std::cout << std::endl;
+//putInt(num2, 10);
+//std::cout << std::endl;
+//putInt(num3, 10);
+//std::cout << std::endl;
