cc4c99b5deb97a7a11db8702c76c2513fdcff5c4
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-23 07:26:54 -0800

    Input Scores

--- Homeworks/HW2/Scores/Scores/Input_Scores.hpp
+++ Homeworks/HW2/Scores/Scores/Input_Scores.hpp
@@ -8,7 +8,38 @@
 
 #ifndef Input_Scores_hpp
 #define Input_Scores_hpp
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 #include <stdio.h>
+#include <utility>
+
+std::vector<std::pair<std::string, int>> Input_Scores(std::vector<std::pair<std::string, int>> Names_Scores){
+        
+    
+    bool runtime = true;
+   std::string Input_Name;
+   int Input_Score;
+   while(runtime == true){
+        std::cout << "Please enter a Name and then a Score:";
+        std::cin >> Input_Name;
+        std::cin >> Input_Score;
+        if(Input_Name == "NoName" && Input_Score == 0){
+            break;
+        }
+        else{
+       Names_Scores.emplace_back(Input_Name, Input_Score);
+            
+        }
+            
+    }
+        sort( Names_Scores.begin(), Names_Scores.end() );
+
+    return Names_Scores;
+
+}
+
+
 
 #endif /* Input_Scores_hpp */
--- Homeworks/HW2/Scores/Scores/main.cpp
+++ Homeworks/HW2/Scores/Scores/main.cpp
@@ -7,9 +7,83 @@
 //
 
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <string>
+#include "Input_Scores.hpp"
+#include <utility>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    std::vector<std::pair<std::string, int>> Names_Scores;
+    Names_Scores = Input_Scores(Names_Scores);
+   
+   
+    
+    
+    
+    
+    bool MenuRuntime = true;
+    while (MenuRuntime == true){
+    int MenuSelection;
+    std::cout << "Please select from the menu below,\n";
+    
+    std::cout << "1: Add More Names and Scores \n";
+    std::cout << "2: Print the Names and Scores \n";
+    std::cout << "3: Search for a Name \n";
+    std::cout << "4: Search for a Score\n";
+    std::cin >> MenuSelection;
+    if (MenuSelection == 1){
+    Names_Scores = Input_Scores(Names_Scores);
+    }
+    else if (MenuSelection == 2){
+        for(int i = 0; i < Names_Scores.size(); i++)
+        {
+             std::cout << Names_Scores[i].first << ", " << Names_Scores[i].second << std::endl;
+        }
+    }
+    else if (MenuSelection == 3){
+        std::string nameToFind;
+        std::cout << "Enter the name you want to search: ";
+        std::cin >> nameToFind;
+        int i = 0;
+        int counter=0;
+        while (i <= Names_Scores.size()){
+                std::string cName = Names_Scores[i].first;
+                if (cName == nameToFind){
+                     std::cout << Names_Scores[i].first << ", " << Names_Scores[i].second << std::endl;;
+                    counter++;
+                }
+                i++;
+            }
+        if (counter==0){
+            std::cout <<"Unable to find Name!! \n";
+        
+        }
+    }
+    else if (MenuSelection == 4){
+        int scoreToFind;
+        std::cout << "Enter the score you want to search: ";
+        std::cin >> scoreToFind;
+        int i = 0;
+        int counter=0;
+        while (i <= Names_Scores.size()){
+                int cScore = Names_Scores[i].second;
+                if (cScore == scoreToFind){
+                     std::cout << Names_Scores[i].first << ", " << Names_Scores[i].second << std::endl;;
+                    counter++;
+                }
+                i++;
+            }
+        if (counter==0){
+            std::cout <<"Unable to find Score!! \n";
+        
+        }
+        
+    }
+    else{
+        std::cout << "Exiting program!,\n";
+        MenuRuntime = false;
+        
+    }
+    
+    }
 }
