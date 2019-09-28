f9cad9c4b39cd3fd6ad36f054c86c31b5f14bbda
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-09-28 15:37:06 -0800

    boxer funciton created

---
+++ Homeworks/HW3/boxer/boxer/boxer.cpp
@@ -0,0 +1,9 @@
+//
+//  boxer.cpp
+//  boxer
+//
+//  Created by Stefano Fochesatto on 9/28/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#include "boxer.hpp"
---
+++ Homeworks/HW3/boxer/boxer/boxer.hpp
@@ -0,0 +1,14 @@
+//
+//  boxer.hpp
+//  boxer
+//
+//  Created by Stefano Fochesatto on 9/28/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#ifndef boxer_hpp
+#define boxer_hpp
+
+#include <stdio.h>
+
+#endif /* boxer_hpp */
