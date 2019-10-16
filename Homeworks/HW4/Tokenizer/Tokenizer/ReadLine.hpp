84d945e383266b71bf4873f84e75218465d2a560
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-10-15 20:03:00 -0800

    ReadLine implimented

---
+++ Homeworks/HW4/Tokenizer/Tokenizer/ReadLine.hpp
@@ -0,0 +1,14 @@
+//
+//  ReadLine.hpp
+//  Tokenizer
+//
+//  Created by Stefano Fochesatto on 10/15/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#ifndef ReadLine_hpp
+#define ReadLine_hpp
+
+#include <stdio.h>
+
+#endif /* ReadLine_hpp */
---
+++ Homeworks/HW4/Tokenizer/Tokenizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tokenizer
+//
+//  Created by Stefano Fochesatto on 10/15/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
