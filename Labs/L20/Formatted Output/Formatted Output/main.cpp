d8ec1f27bad41e99b02d1c92cad7202080b2cb32
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-10-21 10:27:39 -0800

    Lab 20 completed

--- Labs/L20/Formatted Output/Formatted Output/main.cpp
+++ Labs/L20/Formatted Output/Formatted Output/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
-
+#include <cmath>
+#include <sstream>
+#include <iomanip>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    
+    
+    
+    for (int i = 0; i<=36; i++){
+    double param, result, exponential;
+      param = 5 * i;
+   
+    std::ostringstream oss1;
+    
+    
+      result = cos ( param * M_PI / 180.0);
+    oss1 << std::setw(3) <<param << "  " <<std::fixed <<std::setprecision(8) << std::setw(11) << result << std::endl;
+    
+    std::cout << oss1.str();
+        
+    }
+     for (int j = 0; j<=36; j++){
+             double param, exponential;
+              exponential = exp(param / 10);
+               param = 5 * j;
+          std::ostringstream oss2;
+    
+        oss2 << std::setw(3) << param << "  " <<std::fixed <<std::setprecision(2) << std::setw(11) << exponential << std::endl;
+         
+         std::cout << oss2.str();
+     }
+    
     return 0;
 }
+
