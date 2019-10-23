2682cc7d34aa6653b451a5516ef8012094b42964
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-10-23 10:26:09 -0800

    Lab 21

---
+++ Labs/L21/Programming with struct/Programming with struct/truncstruct.cpp
@@ -0,0 +1,9 @@
+//
+//  truncstruct.cpp
+//  Programming with struct
+//
+//  Created by Stefano Fochesatto on 10/23/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#include "truncstruct.hpp"
---
+++ Labs/L21/Programming with struct/Programming with struct/truncstruct.hpp
@@ -0,0 +1,14 @@
+//
+//  truncstruct.hpp
+//  Programming with struct
+//
+//  Created by Stefano Fochesatto on 10/23/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#ifndef truncstruct_hpp
+#define truncstruct_hpp
+
+#include <stdio.h>
+
+#endif /* truncstruct_hpp */
