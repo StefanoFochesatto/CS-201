a5be404cb29b65ef4eca8bda7bea3708a08ae578
Stefano Fochesatto <stefano.gfochesatto@gmail.com>
2019-10-26 15:16:00 -0800

    sequential search with pair lab completed

---
+++ Labs/L22/Sequential search with pair/Sequential search with pair/lookup2.cpp
@@ -0,0 +1,9 @@
+//
+//  lookup2.cpp
+//  Sequential search with pair
+//
+//  Created by Stefano Fochesatto on 10/25/19.
+//  Copyright © 2019 Stefano Fochesatto. All rights reserved.
+//
+
+#include "lookup2.hpp"
